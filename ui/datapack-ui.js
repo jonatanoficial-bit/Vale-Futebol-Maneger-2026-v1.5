809b3ba3cfb786e68a62e24a81885ea2bb60376b
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-01-06 17:11:25 -0300

    Delete ui directory

--- ui/datapack-ui.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  // Arquivo mantido apenas para compatibilidade com o index.html.
-  // A tela de DataPack está implementada em ui/ui.js (rota: 'datapack').
-})();
--- ui/saves-ui.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  // Arquivo mantido apenas para compatibilidade com o index.html.
-  // A tela de Saves/Slots está implementada em ui/ui.js (rota: 'saves').
-})();
