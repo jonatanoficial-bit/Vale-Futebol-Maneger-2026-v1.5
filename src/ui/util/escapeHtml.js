82d9619aa5871857c7843f3021dbce1aac6c3b31
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-01-09 13:58:58 -0300

    Update escapeHtml.js

--- src/ui/util/escapeHtml.js
+++ src/ui/util/escapeHtml.js
@@ -1,12 +1,15 @@
-// src/ui/util/escapeHtml.js
-export function escapeHtml(input = "") {
-  const s = String(input);
-  return s
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#039;");
+// /src/ui/util/escapeHtml.js
+export function escapeHtml(input) {
+  const s = String(input ?? "");
+  return s.replace(/[<>&"]/g, (c) => {
+    switch (c) {
+      case "<": return "&lt;";
+      case ">": return "&gt;";
+      case "&": return "&amp;";
+      case '"': return "&quot;";
+      default: return c;
+    }
+  });
 }
 
 export default escapeHtml;
