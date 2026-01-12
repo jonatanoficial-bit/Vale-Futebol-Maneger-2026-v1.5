534f704db9e7451e66ae9a718cba4b0aa30eb321
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-01-12 19:17:14 -0300

    Criar bootstrap.js

---
+++ bootstrap.js
@@ -0,0 +1,5 @@
+// bootstrap.js (RAIZ)
+// Este arquivo é só um "loader" para garantir que GitHub Pages e Vercel
+// carreguem o app sempre do mesmo jeito, sem tentar importar arquivos inexistentes.
+
+import "./src/app/bootstrap.js";
